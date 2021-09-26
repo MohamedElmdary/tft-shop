6b51848fa58757b2a01b8ced489fd712123ec5cc
MohamedElmdary <engm5081@gmail.com>
2021-09-26 09:41:10 +0200

    Added shop ui

---
+++ src/components/TradeForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <form>
+    <v-text-field
+      label="Bit Coin"
+      color="indigo"
+      type="number"
+      min="0"
+      v-model="form.bitcoins"
+      @input="updateTFTCoins()"
+    >
+      <v-img slot="append" src="@/assets/bitcoin.svg" width="20" />
+    </v-text-field>
+
+    <v-text-field
+      label="TFT Coin"
+      color="indigo"
+      type="number"
+      min="0"
+      disabled
+      v-model="form.tftcoins"
+    >
+      <v-img slot="append" src="@/assets/tft.png" width="20" />
+    </v-text-field>
+
+    <v-text-field
+      label="TFT Wallet Address"
+      color="indigo"
+      type="text"
+      v-model="form.address"
+    >
+      <v-icon slot="append" color="red">
+        mdi-map-marker
+      </v-icon>
+    </v-text-field>
+
+    <v-btn color="indigo">
+      Swap BitCoins into TFT
+    </v-btn>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "TradeForm",
+})
+export default class TradeForm extends Vue {
+  form = {
+    bitcoins: 0,
+    tftcoins: 0,
+    address: "",
+  };
+
+  public updateTFTCoins(): void {
+    this.form.tftcoins = this.form.bitcoins * 1715;
+  }
+}
+</script>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -7,17 +7,27 @@
     <v-divider class="mt-4 mb-4" />
 
     <v-row>
-      <v-col :cols="4">1</v-col>
-      <v-col :cols="8">2</v-col>
+      <v-col :cols="6">
+        <v-row justify="center">
+          <v-img src="@/assets/trade.svg" max-width="600px" width="90%" />
+        </v-row>
+      </v-col>
+      <v-col :cols="6">
+        <TradeForm />
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import TradeForm from "@/components/TradeForm.vue";
 
 @Component({
   name: "Shop",
+  components: {
+    TradeForm,
+  },
 })
 export default class Shop extends Vue {}
 </script>
